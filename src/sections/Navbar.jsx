972ed8388145f169d7797787cacbb859d21cc36d
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-27 12:19:25 +0500

    feat: add responsive Navbar component

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { navLinks } from '../constants/index.js';
 
 const NavItems = ({ onClick = () => {} }) => (
-  <ul className="nav-ul">
+  <ul className="flex flex-col items-center gap-4 sm:flex-row md:gap-6 relative z-20">
     {navLinks.map((item) => (
       <li key={item.id} className=" text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
         <a href={item.href} className="text-lg md:text-base hover:text-white transition-colors" onClick={onClick}>
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between items-center py-5 mx-auto c-space">
+        <div className="flex justify-between items-center py-3 px-2 mx-auto c-space">
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
             Haseeb
           </a>
