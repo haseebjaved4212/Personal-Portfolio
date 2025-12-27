46299fbaa40f99ad229df719c9a430cd6498b76d
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-27 12:14:07 +0500

    feat: Initialize application with a responsive Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from "./sections/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -5,8 +5,8 @@ import { navLinks } from '../constants/index.js';
 const NavItems = ({ onClick = () => {} }) => (
   <ul className="nav-ul">
     {navLinks.map((item) => (
-      <li key={item.id} className="nav-li">
-        <a href={item.href} className="nav-li_a" onClick={onClick}>
+      <li key={item.id} className=" text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
+        <a href={item.href} className="text-lg md:text-base hover:text-white transition-colors" onClick={onClick}>
           {item.name}
         </a>
       </li>
@@ -25,7 +25,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
-            Adrian
+            Haseeb
           </a>
 
           <button
@@ -41,7 +41,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+      <div className={`absolute left-0 right-0 bg-black-200 backdrop-blur-sm transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
         <nav className="p-5">
           <NavItems onClick={closeMenu} />
         </nav>
